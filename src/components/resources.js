b6641c7448bf05fa97e5117832e377c8af1a87b9
Sarah Healey <s.deane.healey@gmail.com>
2016-11-11 10:51:14 -0700

    navbar is functioning

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+    Super Special Recipe
+    <ul>
+    <li>1 cup toads</li>
+    <li>1 cup flowers</li>
+    <li>1 cup strawberries</li>
+    <li>1 cup socks</li>
+    </ul>
+    </div>
+  )
+}
